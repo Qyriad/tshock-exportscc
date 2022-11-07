dac769e14298ad9227503942467c9ec57337c190
Mikaela Szekely <mikaela.szekely@qyriad.me>
2022-11-06 20:10:51 -0700

    add initial plugin skeleton

--- CloudSaves/CloudSaves.cs
+++ CloudSaves/CloudSaves.cs
@@ -5,7 +5,25 @@ using TShockAPI;
 
 namespace CloudSaves;
 
-public class CloudSaves
+/// <summary>
+/// The main plugin class should always be decorated with an ApiVersion attribute. The current API version is 1.25.
+[ApiVersion(2, 1)]
+public class CloudSaves : TerrariaPlugin
 {
+	public override string Name => "CloudSaves";
 
+	public override Version Version => new Version(0, 1, 0);
+
+	public override string Author => "Qyriad";
+
+	public override string Description => "Conditional server-side characters to emulate cloud saves";
+
+	public CloudSaves(Main game) : base(game)
+	{
+	}
+
+	public override void Initialize()
+	{
+		TShock.Log.ConsoleInfo("Initialize()!");
+	}
 }
